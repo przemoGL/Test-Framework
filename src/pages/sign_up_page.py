20f9fcb06015f8f358a29fa90029119dc1d66b95
Przemysław Szpak <przemyslaw.szpak@globallogic.com>
2022-11-28 14:45:33 +0100

    Add GitHub sing up pages models with test

---
+++ src/pages/sign_up_page.py
@@ -0,0 +1,23 @@
+from src.config.config import config
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class SignUpPage:
+
+    LOGIN_URL = config.GITHUB_MAIN_URL + "/signup"
+
+    email_field = (By.ID, "email")
+    continue_button = (By.CLASS_NAME, "js-continue-button")
+
+    def __init__(self, github_ui_app):
+        self.github_ui_app = github_ui_app
+
+    def open_sing_up_panel(self):
+        self.github_ui_app.open_page(self.LOGIN_URL)
+
+    def fill_email(self, email):
+        WebDriverWait(self.github_ui_app.driver, 10).until(EC.visibility_of_element_located(self.email_field))
+        self.github_ui_app.fill_field_by_text(*self.email_field, email)
+        self.github_ui_app.click(*self.continue_button)
