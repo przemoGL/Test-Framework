b0bc935ca85ae62b831b80dedd2b06833635a9e8
Przemysław Szpak <przemyslaw.szpak@globallogic.com>
2022-12-08 20:33:10 +0100

    Add full tests report and fix test GitHub API test

--- tests/test_github_api.py
+++ tests/test_github_api.py
@@ -9,7 +9,7 @@ class TestsGitHubAPI:
         body = github_api.get_repository_info(repository_path)
         assert body['items'][0]['name'] == "Test-Framework"
 
-    @pytest.mark.parametrize("username, username_path_param, sort_by",
+    @pytest.mark.parametrize("username, username_path_param, sort",
                              [["pszpak", "przemoGL", "created"]])
     def test_repositories_order(self, github_api, username_path_param, sort):
         body = github_api.get_user_repositories(username_path_param, sort)
@@ -17,6 +17,6 @@ class TestsGitHubAPI:
 
     @pytest.mark.parametrize("username, username_path_param, sort, direction",
                              [["pszpak", "przemoGL", "created", "desc"]])
-    def test_repositories_order(self, github_api, username_path_param, sort, direction):
+    def test_repositories_sorting_direction(self, github_api, username_path_param, sort, direction):
         body = github_api.get_user_repositories(username_path_param, sort, direction)
         assert body[0]['id'] > body[1]['id']
